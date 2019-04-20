c39713793d025656cd5e937d0876783f13129be4
Jhon Billy <jhonbilly@billy.com.br>
2019-04-20 12:35:42 -0300

    clean code e troca de variaveis

---
+++ ValidadorCPF/ValidadorCPF/Validador.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ValidadorCPF
+{
+    partial class frmValidadorCPF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCPF.Location = new System.Drawing.Point(68, 67);
+            this.txtCPF.Multiline = true;
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(193, 27);
+            this.txtCPF.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(68, 136);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(193, 33);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Consultar";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Location = new System.Drawing.Point(65, 51);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(74, 13);
+            this.lblCPF.TabIndex = 2;
+            this.lblCPF.Text = "Informe o CPF";
+           // this.lblCPF.Click += new System.EventHandler(this.lblCPF_Click);
+            // 
+            // frmValidadorCPF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 223);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtCPF);
+            this.Name = "frmValidadorCPF";
+            this.Text = "Validador CPF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox txtCPF;
+        public System.Windows.Forms.Button btnCalcular;
+        public System.Windows.Forms.Label lblCPF;
+    }
+}
+
---
+++ ValidadorCPF/ValidadorCPF/Validador.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidadorCPF
+{
+    public partial class frmValidadorCPF : Form
+    {
+        public frmValidadorCPF()
+        {
+            InitializeComponent();
+        }
+        
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string cpf = txtCPF.Text;
+            int amountCharacterCPF = cpf.Length;
+
+            if (amountCharacterCPF == 11)
+            {
+                var sum = 0;
+                var multiplier = 10;
+
+                for (int counter = 0; counter < 9; counter++)
+                {
+                    int number = int.Parse(cpf.Substring(counter, 1)) * multiplier;
+                    sum = sum + number;
+                    multiplier--;
+
+                }
+
+                var division1 = sum % 11;
+                var digit = 11 - division1;
+
+                if (digit > 9)
+                    digit = 0;
+
+                var sum2 = 0;
+                multiplier = 11;
+                for (int counter = 0; counter < 9; counter++)
+                {
+                    int number = int.Parse(cpf.Substring(counter, 1)) * multiplier;
+                    sum2 = sum2 + number;
+                    multiplier--;
+                }
+
+                var numero10 = digit * 2;
+                sum2 = sum2 + numero10;
+
+                var division2 = sum2 % 11;
+                var digit2 = 11 - division2;
+
+                if (digit2 > 9)
+                    digit2 = 0;
+
+                if (digit == int.Parse(cpf.Substring(9, 1)) && digit2 == int.Parse(cpf.Substring(10, 1)))
+                    MessageBox.Show("CPF Valido!!");
+                else
+                    MessageBox.Show("CPF Invalido!!");
+
+            }
+            else
+            {
+                MessageBox.Show("Informe um CPF com 11 caracteres");
+            }
+        }
+    }
+}
